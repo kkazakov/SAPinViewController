500ac36cab692f22b00f6b81e806a7df9963769a
Krasimir Kazakov <kkazakov@gmail.com>
2019-01-10 16:17:50 +0200

    Reverted to older swift ...

--- SAPinViewController/Classes/SAPinViewController.swift
+++ SAPinViewController/Classes/SAPinViewController.swift
@@ -299,7 +299,7 @@ open class SAPinViewController: UIViewController {
             })
         }
         view.addSubview(blurView)
-        view.bringSubviewToFront(blurView)
+        view.bringSubview(toFront: blurView)
         blurView.snp.makeConstraints { (make) in
             make.edges.equalTo(self.view)
         }
@@ -538,7 +538,7 @@ open class SAPinViewController: UIViewController {
         return UIScreen.main.bounds.height == 480
     }
     fileprivate func setAttributedTitleForButtonWithTitle(_ title: String, font: UIFont, color: UIColor) {
-        cancelButton.setAttributedTitle(NSAttributedString(string: title, attributes: convertToOptionalNSAttributedStringKeyDictionary([convertFromNSAttributedStringKey(NSAttributedString.Key.font):font,convertFromNSAttributedStringKey(NSAttributedString.Key.foregroundColor):color])), for: UIControl.State())
+        cancelButton.setAttributedTitle(NSAttributedString(string: title, attributes: [NSFontAttributeName:font,NSForegroundColorAttributeName:color]), for: UIControl.State())
     }
     fileprivate func pinErrorAnimate() {
         for item in circleViews {
@@ -637,15 +637,4 @@ extension SAPinViewController: SAButtonViewDelegate {
             }
         }
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertToOptionalNSAttributedStringKeyDictionary(_ input: [String: Any]?) -> [NSAttributedString.Key: Any]? {
-	guard let input = input else { return nil }
-	return Dictionary(uniqueKeysWithValues: input.map { key, value in (NSAttributedString.Key(rawValue: key), value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromNSAttributedStringKey(_ input: NSAttributedString.Key) -> String {
-	return input.rawValue
 }
